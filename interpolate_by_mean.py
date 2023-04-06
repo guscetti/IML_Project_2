a01b32bbb8edde373d145e61d2559d33cf35f4d7
studentethz <117648189+studentethz@users.noreply.github.com>
2023-04-06 19:01:48 +0200

    höde
    grossi höde

--- interpolate_by_mean.py
+++ interpolate_by_mean.py
@@ -34,6 +34,23 @@ def interpolate_x(johresziit):
                 data_season.loc[i, spaltenname] = mean
     return data_season
 
+def interpolate_x_no_season(johresziit):
+    data = pd.read_csv("train.csv")
+    data = data.drop(columns=["season"])
+
+    #delete entries with y = NaN
+    for i in list(data.index.values):
+        if pd.isna(data.loc[i, 'price_CHF']):
+            data = data.drop(i)
+
+    #set entries with X = NaA to X = X_mean
+    for spaltenname in data.columns:
+        mean = data[spaltenname].mean()
+        for i in list(data.index.values):
+            if pd.isna(data.loc[i, spaltenname]):
+                data.loc[i, spaltenname] = mean
+    return data
+
 
 #split the interpolated data frames into numpy array of X and y
 def x_to_np(season, interp):
@@ -53,3 +70,5 @@ def y_to_np(season, interp):
     y = prices_season["price_CHF"].to_numpy()
     return y
 
+
+print(x_to_np('spring', 'only_x'))
